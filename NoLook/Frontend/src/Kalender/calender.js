fdac48c3cf228ba23a70007f9665e1e89c410990
Basti029 <102850316+Basti029@users.noreply.github.com>
2022-05-07 09:55:47 +0200

    calender fix

--- NoLook/Frontend/src/Kalender/calender.js
+++ NoLook/Frontend/src/Kalender/calender.js
@@ -37,6 +37,77 @@ export default class PageEdit extends Page {
     async init() {
         // HTML-Inhalt nachladen
         await super.init();
-        /////////////////////////////////////////////////////
+        this.restart();
+
+        let leftButton = this._mainElement.querySelector("#moveLeft");
+        leftButton.addEventListener("click", () => this.moveLeft());
+
+        let rightButton = this._mainElement.querySelector("#moveRight");
+        rightButton.addEventListener("click", () => this.moveRight());
+
+    }
+
+    restart() {
+        dateValue(); 
+        clear(); 
+        draw();
+    }
+    
+    moveLeft() {
+        date.setMonth(date.getMonth() - 1);
+        dateValue();
+        clear();
+        draw();
+    }
+    
+    moveRight() {
+        date.setMonth(date.getMonth() + 1);
+        dateValue();
+        clear();
+        draw();
+    }
+    
+    dateValue() {
+        monat = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Okotber", "November", "Dezember",]
+        ausgabe = monat[date.getMonth()] + " " + date.getFullYear();
+        this._mainElement.querySelector("#month").textContent = ausgabe;
+    }
+    
+    draw() {
+        var helpDate = new Date(date.getFullYear(), date.getMonth(), 1);
+        var grey = 1;
+        var start = 1;
+        vergleich = [6, 0, 1, 2, 3, 4, 5];
+        start = start + vergleich[helpDate.getDay()];
+        var id = "";
+        while (grey  < start) {
+            id = "#calendar_entry_" + grey;
+            this._mainElement.querySelector(id).style.backgroundColor = "#6d6d6d";
+            grey = grey + 1;
+        }
+        var counter = 0;
+        while (helpDate.getMonth() == date.getMonth()) {
+            counter = counter + 1;
+            id = "calendar_entry_" + start;
+            this._mainElement.querySelector(id).textContent = counter;
+            start = start + 1;
+            helpDate.setDate(helpDate.getDate() + 1); 
+        }
+        while (start < 43) {
+            id = "calendar_entry_" + start;
+            this._mainElement.querySelector(id)(id).style.backgroundColor = "#6d6d6d";
+            start = start + 1;
+        }
+    }
+    
+    clear() {
+        var counter = 1;
+        var id = "";
+        while (counter < 43) {
+            id = "calendar_entry_" + counter;
+            this._mainElement.querySelector(id).textContent = "";
+            this._mainElement.querySelector(id).style.backgroundColor = "white";
+            counter = counter + 1;
+        }
     }
 };
